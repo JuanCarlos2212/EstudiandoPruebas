f37fcd374d25170ba00340139695f97c9a05cc2b
JuanCarlos2212 <75186135@continental.edu.pe>
2022-06-30 20:09:52 -0500

    TomandoExamenes

---
+++ app/src/main/java/com/acevedo/estudiando/MainActivity.java
@@ -0,0 +1,165 @@
+package com.acevedo.estudiando;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceIdReceiver;
+import com.google.firebase.iid.internal.FirebaseInstanceIdInternal;
+import com.google.firebase.installations.FirebaseInstallations;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtUsuario, edtPassword;
+    Button btnLogin,btnLoginEstudiante;
+    String usuario,password;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        setTheme(R.style.Theme_Estudiando); // nuevo
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edtUsuario=findViewById(R.id.edtUsuario);
+        edtPassword=findViewById(R.id.edtPassword);
+        btnLogin=findViewById(R.id.btnLogin);
+        btnLoginEstudiante=findViewById(R.id.btnLoginEstudiante);
+
+        recuperarPregerencias();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                usuario = edtUsuario.getText().toString();
+                password = edtPassword.getText().toString();
+                if(!usuario.isEmpty() && !password.isEmpty()){
+                    validarUsuario("https://pruebasphaway.000webhostapp.com/android/validar_usuario.php");
+                }else{
+                    Toast.makeText(MainActivity.this,"No se permiten campos vacios",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+        btnLoginEstudiante.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                usuario = edtUsuario.getText().toString();
+                password = edtPassword.getText().toString();
+                if(!usuario.isEmpty() && !password.isEmpty()){
+                    validarEstudiante("https://pruebasphaway.000webhostapp.com/android/validar_estudiante.php");
+                }else{
+                    Toast.makeText(MainActivity.this,"No se permiten campos vacios",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+    private void validarUsuario(String URL){
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if(response.length()>=15){
+                    guardarPreferencias();
+                    Intent intent = new Intent(getApplicationContext(), PrincipalActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else {
+                    Toast.makeText(MainActivity.this,"usuario o contraseña incorrecta",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this,error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                // Map<String,String> parametros = new HashMap<String,String>();
+                Map<String,String> parametros = new HashMap<>();
+                parametros.put("usuario",usuario);
+                parametros.put("password",password);
+                return parametros;
+            }
+        };
+
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+    private void validarEstudiante(String URL){
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if(response.length()>=15){
+                    guardarPreferencias();
+                    Intent intent = new Intent(getApplicationContext(), PrincipalEstudianteActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else {
+                    Toast.makeText(MainActivity.this,"usuario o contraseña incorrecta",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this,error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                // Map<String,String> parametros = new HashMap<String,String>();
+                Map<String,String> parametros = new HashMap<>();
+                parametros.put("usuario",usuario);
+                parametros.put("password",password);
+                return parametros;
+            }
+        };
+
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    private void guardarPreferencias(){
+        SharedPreferences preferences=getSharedPreferences("preferenciasLogin", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("usuario",usuario);
+        editor.putString("password",password);
+        editor.putBoolean("session",true);
+        editor.commit();
+    }
+    private void recuperarPregerencias(){
+        SharedPreferences preferences=getSharedPreferences("preferenciasLogin", Context.MODE_PRIVATE);
+        edtUsuario.setText(preferences.getString("usuario","78715868"));
+        edtPassword.setText(preferences.getString("password","78715868SOTO"));
+    }
+}
